93bb6fe4b416d39dc12b8db98883f4c0db7daeb7
KapokaC <charitykapoka@hotmail.co.uk>
2023-10-15 16:32:20 +0100

    color, theme, dark mode, styling setup and navbar done

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,5 +1,11 @@
+import Navbar from "scenes/navbar";
+
+import { Box } from "@mui/material";
+
 const HomePage = () => {
-    return(<div>HomePage</div>)
+    return <Box>
+        <Navbar />
+    </Box>
 }
 
 export default HomePage;
