c5cfccc62dd9daedb0242287f2d48f23e65b6e2f
KapokaC <charitykapoka@hotmail.co.uk>
2023-10-24 17:10:37 +0100

    post and post widgets done

--- client/src/App.js
+++ client/src/App.js
@@ -9,25 +9,27 @@ import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
 
 function App() {
-
   const mode = useSelector((state) => state.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]); 
-   const isAuth = Boolean(useSelector((state) => state.token));
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className="app">
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
               path="/home"
               element={isAuth ? <HomePage /> : <Navigate to="/" />}
             />
-          <Route path="/profile/:userID" element={ <ProfilePage/>} />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
           </Routes>
-          </ThemeProvider>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
